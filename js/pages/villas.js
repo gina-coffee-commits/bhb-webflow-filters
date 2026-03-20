381584c585b8e8479dd2440b771293cb9549df56
gina-coffee-commits <ginavirtualspark@gmail.com>
2026-03-21 05:14:58 +0800

    villas logic updates

--- js/pages/villas.js
+++ js/pages/villas.js
@@ -1609,6 +1609,7 @@
   function buildUI() {
     var root = document.getElementById('bhb-filter');
     if (!root) return;
+    root.innerHTML = '';
 
     var CLOSE_SVG   = '<svg width="14" height="14" viewBox="0 0 14 14" fill="none"><path d="M1 1L13 13M13 1L1 13" stroke="currentColor" stroke-width="1.8" stroke-linecap="round"/></svg>';
     var CHEVRON_SVG = '<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>';
